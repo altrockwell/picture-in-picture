b797ec9c8aaab423c384134c3467092bb09ba80d
Jonathan Aplacador <aplacadorjonathan2@gmail.com>
2022-07-08 13:39:18 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hello world');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('btn');
+
+// Prompt to select media stream, pass to video element then play
+async function selectMediaStream() {
+	try {
+		const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+		videoElement.srcObject = mediaStream;
+		videoElement.onloadedmetadata = () => {
+			videoElement.play();
+		};
+	} catch (error) {
+		console.log('whoops error here: ', error);
+	}
+}
+
+button.addEventListener('click', async () => {
+	// Disable button
+	button.disabled = true;
+	// start picture in picture
+	await videoElement.requestPictureInPicture();
+	// reset button
+	button.disabled = false;
+});
+
+// On load
+selectMediaStream();
